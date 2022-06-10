81020217233c2e0b2c6f5ebc3555b770d0de9ec9
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-06-10 14:58:45 -0300

    add model create product

--- src/models/products.ts
+++ src/models/products.ts
@@ -1,14 +1,26 @@
+import { ResultSetHeader } from 'mysql2/promise';
 import connection from './connection';
 import Product from '../interfaces/product.interface';
 
 const getAll = async () => {
-  const QUERRY_GET_ALL = 'SELECT * FROM Trybesmith.Products';
+  const QUERY_GET_ALL = 'SELECT * FROM Trybesmith.Products';
 
-  const [products] = await connection.execute(QUERRY_GET_ALL);
+  const [products] = await connection.execute(QUERY_GET_ALL);
 
   return products as Product[];
 };
 
+const create = async (product: Product) => {
+  const { name, amount } = product;
+  const QUERY_CREATE = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)';
+  
+  const newProduct = await connection.execute<ResultSetHeader>(QUERY_CREATE, [name, amount]);
+  const [dataInserted] = newProduct;
+  const { insertId } = dataInserted;
+  return { id: insertId, ...product };
+};
+
 export default {
   getAll,
+  create,
 };
